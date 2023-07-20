d38d573cf2e491c025472d01a056067ac09f1fa5
Mohamed Keshawy <mokeshawy@gmail.com>
2023-07-20 06:27:49 +0300

    Update implement Score app

--- app/src/main/java/com/example/firstappcompose/MainActivity.kt
+++ app/src/main/java/com/example/firstappcompose/MainActivity.kt
@@ -5,7 +5,10 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -16,36 +19,49 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            var score by remember { mutableStateOf(0) }
-            ScoreSection(
-                score = score,
-                countCallBack = { score += 1 },
-                minusCountCallBack = { if (score > 0) score -= 1 })
+            ScoreSection()
         }
     }
 }
 
 @Composable
-fun ScoreSection(score: Int, countCallBack: () -> Unit, minusCountCallBack: () -> Unit) {
+fun ScoreSection() {
+    var score by remember { mutableStateOf(0) }
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
-    ){
+    ) {
         Text(
-            color = colorResource(id = R.color.teal_700),
+            style = TextStyle(fontSize = 30.sp),
+            color = colorResource(id = R.color.black),
             text = "Source: $score"
         )
-        Button(onClick = { countCallBack() }) {
+
+        Spacer(modifier = Modifier.height(50.dp))
+
+        Button(
+            modifier = Modifier.width(200.dp).height(50.dp),
+            onClick = { score += 1 }
+        ) {
             Text(text = "Add Points")
         }
 
-        Button(onClick = { minusCountCallBack() }) {
+        Spacer(modifier = Modifier.height(10.dp))
+
+        Button(
+            modifier = Modifier.width(200.dp).height(50.dp),
+            enabled = score > 0,
+            onClick = { if (score > 0) score -= 1 }
+        ) {
             Text(text = "Minus Points")
         }
     }

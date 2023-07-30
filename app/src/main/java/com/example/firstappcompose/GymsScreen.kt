1e2851fad677767ccf1fe81a2f025a3a8dff8b90
mohamed <mohamed.keshawy@solarus-egypt.com>
2023-07-30 07:05:59 +0300

    implement new composable with Hoisting concept

--- app/src/main/java/com/example/firstappcompose/GymsScreen.kt
+++ app/src/main/java/com/example/firstappcompose/GymsScreen.kt
@@ -1,21 +1,26 @@
 package com.example.firstappcompose
 
-import android.widget.Toast
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.material.icons.filled.Place
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -28,44 +33,38 @@ import com.example.GymModel
 import com.example.firstappcompose.ui.theme.Purple40
 
 
-
 @Composable
-fun GymList(){
-    val viewModel : GymViewModel = viewModel()
+fun GymList(onItemCLicked: (GymModel, Int) -> Unit) {
+    val viewModel: GymViewModel = viewModel()
     LazyColumn {
         item {
-            viewModel.getGymList().forEach {
-                GymItem(it) { gymModel ->
-
+            viewModel.getGymList().forEachIndexed { index, gymModel ->
+                GymItem(gymModel) {
+                    onItemCLicked(it, index)
                 }
             }
         }
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
+
 @Composable
 fun GymItem(gymModel: GymModel, onItemCLicked: (GymModel) -> Unit) {
+    var isFavoriteChange by remember { mutableStateOf(false) }
+    val icon = if (isFavoriteChange) Icons.Filled.Favorite else Icons.Filled.FavoriteBorder
     Card(
-        onClick = { onItemCLicked(gymModel) },
         elevation = CardDefaults.cardElevation(4.dp),
         modifier = Modifier.padding(8.dp)
     ) {
         Row(verticalAlignment = Alignment.CenterVertically, modifier = Modifier.padding(8.dp)) {
-            GymIcon(Icons.Filled.Place, Modifier.weight(0.15f))
-            GymDetails(gymModel, Modifier.weight(0.85f))
+            DefaultIcon(Icons.Filled.Place, Modifier.weight(0.15f))
+            GymDetails(gymModel, Modifier.weight(0.70f))
+            DefaultIcon(icon, Modifier.weight(0.15f)) {
+                isFavoriteChange = !isFavoriteChange
+                onItemCLicked(gymModel)
+            }
         }
     }
-}
-
-@Composable
-fun GymIcon(vector: ImageVector, modifier: Modifier) {
-    Image(
-        imageVector = vector,
-        contentDescription = "",
-        modifier = modifier,
-        colorFilter = ColorFilter.tint(Color.DarkGray)
-    )
 }
 
 @Composable
@@ -81,4 +80,14 @@ fun GymDetails(gymModel: GymModel, modifier: Modifier) {
             )
         }
     }
+}
+
+@Composable
+fun DefaultIcon(icon: ImageVector, modifier: Modifier, onItemCLicked: () -> Unit = {}) {
+    Image(
+        imageVector = icon,
+        contentDescription = "",
+        modifier = modifier.padding(10.dp).clickable { onItemCLicked() },
+        colorFilter = ColorFilter.tint(Color.DarkGray)
+    )
 }
--- app/src/main/java/com/example/firstappcompose/MainActivity4.kt
+++ app/src/main/java/com/example/firstappcompose/MainActivity4.kt
@@ -4,15 +4,19 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.lazy.LazyColumn
-import com.example.GymModel
 
 class MainActivity4 : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
+            GymList { gym, index ->
+                Toast.makeText(
+                    this,
+                    "Gym name : ${gym.gymName} - Index of item: $index",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
 //            Column (Modifier.verticalScroll(rememberScrollState())){
 //                listOfGym.forEach {
 //                    GymItem(it)

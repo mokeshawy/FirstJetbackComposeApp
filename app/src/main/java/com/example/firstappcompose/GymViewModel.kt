f74f5e29a9b57548cf548e10d5a96f7d67d95fc4
mohamed <mohamed.keshawy@solarus-egypt.com>
2023-08-02 17:04:11 +0300

    implement SavedStateHandle

--- app/src/main/java/com/example/firstappcompose/GymViewModel.kt
+++ app/src/main/java/com/example/firstappcompose/GymViewModel.kt
@@ -3,12 +3,16 @@ package com.example.firstappcompose
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import com.example.GymModel
 import com.example.listOfGym
 
-class GymViewModel : ViewModel() {
+const val GYM_IDS = "GYM_IDS"
 
-    var state by mutableStateOf(getGymList())
+class GymViewModel(private val stateHandle: SavedStateHandle) : ViewModel() {
+
+    var state by mutableStateOf(retrieveGymIds())
 
     private fun getGymList() = listOfGym
 
@@ -16,6 +20,24 @@ class GymViewModel : ViewModel() {
         val list = state.toMutableList()
         val indexOf = list.indexOfFirst { it.id == id }
         list[indexOf] = list[indexOf].copy(isFavorite = !list[indexOf].isFavorite)
+        storeSelectGym(list[indexOf])
         state = list
+    }
+
+    private fun storeSelectGym(gymModel: GymModel) {
+        val savedStateHandle = stateHandle.get<List<Int>?>(GYM_IDS).orEmpty().toMutableList()
+        if (gymModel.isFavorite) savedStateHandle.add(gymModel.id)
+        else savedStateHandle.remove(gymModel.id)
+        stateHandle[GYM_IDS] = savedStateHandle
+    }
+
+    private fun retrieveGymIds(): List<GymModel> {
+        val gym = getGymList()
+        stateHandle.get<List<Int>?>(GYM_IDS)?.let { ids ->
+            ids.forEach { id ->
+                gym.find { it.id == id }?.isFavorite = true
+            }
+        }
+        return gym
     }
 }
